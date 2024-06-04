208d1fb70fcb2ae78c65263154f4a1be5dede8fe
ganghyeon jo <ganghyeon4479@naver.com>
2024-06-04 19:48:32 +0900

    front --프론트 게시글 작성 및 등록 후 boardlist로 이동

--- frontend/src/page/Home.jsx
+++ frontend/src/page/Home.jsx
@@ -1,3 +1,13 @@
+import Navbar from "./for_develop/Navbar.jsx";
+import React from "react";
+import { Outlet } from "react-router-dom";
+import { Box } from "@chakra-ui/react";
+
 export function Home() {
-  return null;
+  return (
+    <Box>
+      <Navbar />
+      <Outlet />
+    </Box>
+  );
 }
--- frontend/src/page/board/BoardWrite.jsx
+++ frontend/src/page/board/BoardWrite.jsx
@@ -1,16 +1,56 @@
-import { Box, FormControl, FormLabel, Input, Textarea } from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  FormControl,
+  FormLabel,
+  Input,
+  Textarea,
+} from "@chakra-ui/react";
+import axios from "axios";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 export function BoardWrite() {
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+  const useToast = useToast();
+  const navigate = useNavigate();
+  function handleSaveClick() {
+    axios
+      .post("/api/board/add", {
+        title,
+        content,
+      })
+      .then(() => {
+        navigate("/boardList");
+      });
+  }
+
   return (
     <Box>
-      <FormControl>
-        <FormLabel>제목</FormLabel>
-        <Input></Input>
-      </FormControl>
-      <FormControl>
-        <FormLabel>내용</FormLabel>
-        <Textarea></Textarea>
-      </FormControl>
+      <Box>
+        <FormControl>
+          <FormLabel>제목</FormLabel>
+          <Input onChange={(e) => setTitle(e.target.value)}></Input>
+        </FormControl>
+      </Box>
+      <Box>
+        <FormControl>
+          <FormLabel>내용</FormLabel>
+          <Textarea onChange={(e) => setContent(e.target.value)}></Textarea>
+        </FormControl>
+      </Box>
+      <Box>
+        <FormControl>
+          <FormLabel>작성자</FormLabel>
+          <Input readOnly value="작성자"></Input>
+        </FormControl>
+      </Box>
+      <Box>
+        <Button colorScheme={"blue"} onClick={handleSaveClick}>
+          저장
+        </Button>
+      </Box>
     </Box>
   );
 }
