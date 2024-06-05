c96ef9b0ecc617ee34a5abb4408ffcc101393903
chaiheung <chaiheung@naver.com>
2024-06-05 19:45:46 +0900

    back: Member / controller, service, mapper, domain 생성

---
+++ backend/src/main/java/com/backend/controller/member/MemberController.java
@@ -0,0 +1,11 @@
+package com.backend.controller.member;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/member")
+public class MemberController {
+}
---
+++ backend/src/main/java/com/backend/service/member/MemberService.java
@@ -0,0 +1,9 @@
+package com.backend.service.member;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class MemberService {
+}
