28c62ea704891ebf5e1f63c23b4356d75682c8cc
ganghyeon jo <ganghyeon4479@naver.com>
2024-06-05 11:03:29 +0900

    backend --board테이블 생성 및 데이터삽입

--- backend/sql/project.sql
+++ backend/sql/project.sql
@@ -1,1 +1,10 @@
-USE
+USE prj3;
+
+CREATE TABLE board
+(
+    id       INT PRIMARY KEY AUTO_INCREMENT,
+    title    VARCHAR(100)  NOT NULL,
+    content  VARCHAR(1000) NOT NULL,
+    writer   VARCHAR(100)  NOT NULL,
+    inserted DATETIME      NOT NULL DEFAULT NOW()
+);
