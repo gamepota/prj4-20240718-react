3c2e9b7f6551b8a8aebe5a6465ba99891cc7b43e
chaiheung <chaiheung@naver.com>
2024-06-09 05:53:03 +0900

    back: BCryptPasswordEncoder 로직 작성

--- backend/src/main/java/com/backend/config/SecurityConfiguration.java
+++ backend/src/main/java/com/backend/config/SecurityConfiguration.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
@@ -19,5 +20,10 @@ public class SecurityConfiguration {
 //        http.oauth2ResourceServer(configurer -> configurer.jwt(Customizer.withDefaults()));
 
         return http.build();
+    }
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
