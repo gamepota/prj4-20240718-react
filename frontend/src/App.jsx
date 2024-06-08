423416386a8c074f18a9efe2bd0009fbab055fe6
chaiheung <chaiheung@naver.com>
2024-06-09 06:06:43 +0900

    front: MemberLogin 생성 및 경로 설정

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,13 +2,17 @@ import React from "react";
 import { ChakraProvider } from "@chakra-ui/react";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { MemberSignup } from "./page/member/MemberSignup.jsx";
+import { MemberLogin } from "./page/member/MemberLogin.jsx";
 import { Home } from "./page/Home.jsx";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Home />,
-    children: [{ path: "member/signup", element: <MemberSignup /> }],
+    children: [
+      { path: "member/signup", element: <MemberSignup /> },
+      { path: "member/login", element: <MemberLogin /> },
+    ],
   },
 ]);
 
---
+++ frontend/src/page/member/MemberLogin.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function MemberLogin(props) {
+  return (
+    <div></div>
+  );
+}
+
+export default MemberLogin;
