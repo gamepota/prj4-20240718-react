5694384a34775c18c13f1fde73065c212bc3a05c
ganghyeon jo <ganghyeon4479@naver.com>
2024-06-10 16:40:56 +0900

    frontend --게시글에서 수정버튼 누르면 수정 페이지로 이동..

--- frontend/src/page/board/BoardEdit.jsx
+++ frontend/src/page/board/BoardEdit.jsx
@@ -1,10 +1,33 @@
-import { Box, FormControl, FormLabel, Input } from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  FormControl,
+  FormLabel,
+  Input,
+  Spinner,
+  Textarea,
+} from "@chakra-ui/react";
 import { useParams } from "react-router-dom";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 export function BoardEdit() {
   const { id } = useParams();
   const [board, setBoard] = useState(null);
+  useEffect(() => {
+    axios.get(`/api/board/${id}`).then((res) => {
+      setBoard(res.data);
+    });
+  }, []);
+  function handleClickSave() {
+    axios.post(`/api/board/edit/`, board);
+  }
+
+  //useEffect가 실행될때까지 스피너 돌아감..
+  if (board === null) {
+    return <Spinner />;
+  }
+
   return (
     <Box>
       <Box>{id}번 게시물 수정</Box>
@@ -16,6 +39,29 @@ export function BoardEdit() {
             onChange={(e) => setBoard({ ...board, title: e.target.value })}
           />
         </FormControl>
+      </Box>
+      <Box>
+        <FormControl>
+          <FormLabel>본문</FormLabel>
+          <Textarea
+            defaultValue={board.content}
+            onChange={(e) => setBoard({ ...board, content: e.target.value })}
+          />
+        </FormControl>
+      </Box>
+      <Box>
+        <FormControl>
+          <FormLabel>작성자</FormLabel>
+          <Input
+            defaultValue={board.writer}
+            onChange={(e) => setBoard({ ...board, writer: e.target.value })}
+          />
+        </FormControl>
+      </Box>
+      <Box>
+        <Button colorScheme={"blue"} onClick={handleClickSave}>
+          수정
+        </Button>
       </Box>
     </Box>
   );
