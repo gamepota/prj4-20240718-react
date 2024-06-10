c198adeac638925906dc2ab4090d7e56b8596850
ganghyeon jo <ganghyeon4479@naver.com>
2024-06-10 18:30:48 +0900

    backend --board테이블 id->board_id로 수정 후 자기 복제하여 레코드 개수 320개로 만듦

--- backend/sql/board.sql
+++ backend/sql/board.sql
@@ -2,7 +2,7 @@ USE prj3;
 
 CREATE TABLE board
 (
-    id       INT PRIMARY KEY AUTO_INCREMENT,
+    board_id INT PRIMARY KEY AUTO_INCREMENT,
     title    VARCHAR(100)  NOT NULL,
     content  VARCHAR(1000) NOT NULL,
     writer   VARCHAR(100)  NOT NULL,
