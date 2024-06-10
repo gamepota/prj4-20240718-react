9e1d08dc0501eec9cf0835e1006aabb04510d71a
chaiheung <chaiheung@naver.com>
2024-06-10 16:23:10 +0900

    back: security 작업을 위한 securityConfig 설정 추가

--- backend/src/main/java/com/backend/config/SecurityConfiguration.java
+++ backend/src/main/java/com/backend/config/SecurityConfiguration.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -14,16 +15,32 @@ import static org.springframework.security.config.Customizer.withDefaults;
 public class SecurityConfiguration {
 
     @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.csrf(csrf -> csrf.disable());
+        http.csrf(auth -> auth.disable());
+        // form login disable
+        http.formLogin(auth -> auth.disable());
+        // http basic disable
+        http.httpBasic(auth -> auth.disable());
+
+        // 경로별 권한
+        http.authorizeHttpRequests((auth) -> auth
+                .requestMatchers("/", "/member/login", "/api/member/signup").permitAll()
+                .requestMatchers("/admin").hasRole("ADMIN")
+                .anyRequest().authenticated());
+
+        //세션 설정
+        http
+                .sessionManagement((session) -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
         http.cors(withDefaults());
 //        http.oauth2ResourceServer(configurer -> configurer.jwt(Customizer.withDefaults()));
 
         return http.build();
-    }
-
-    @Bean
-    public BCryptPasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
     }
 }
