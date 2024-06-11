d80720434734f96c9bee7ac71e7fd34b01f4c96c
ganghyeon jo <ganghyeon4479@naver.com>
2024-06-11 12:12:28 +0900

    frontend --게시물 30개 50개 100개 나눠서 가져오는 ui

--- frontend/src/page/board/BoardList.jsx
+++ frontend/src/page/board/BoardList.jsx
@@ -1,26 +1,93 @@
-import { Box, Center, Table, Tbody, Th, Thead, Tr } from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  Center,
+  Flex,
+  Menu,
+  MenuButton,
+  MenuItem,
+  MenuList,
+  Table,
+  Tbody,
+  Th,
+  Thead,
+  Tr,
+} from "@chakra-ui/react";
 import { useEffect, useState } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
+import { ChevronDownIcon, ChevronUpIcon } from "@chakra-ui/icons";
 
 export function BoardList() {
   const [boardList, setBoardList] = useState([]);
+  const [pageAmount, setPageAmount] = useState(50);
   const navigate = useNavigate();
   useEffect(() => {
-    axios.get("/api/board/list").then((res) => {
-      console.log("API 응답데이터", res.data);
-      // const validData = res.data.filter((board) => board.board_id != null);
-      setBoardList(res.data);
-      console.log(res.data);
-    });
-  }, []);
+    axios
+      .get("/api/board/list", {
+        data: {
+          pageAmount: pageAmount,
+        },
+      })
+      .then((res) => {
+        console.log("API 응답데이터", res.data);
+        // const validData = res.data.filter((board) => board.board_id != null);
+        setBoardList(res.data);
+        console.log(res.data);
+      });
+  }, [pageAmount]);
+
+  function handlePageSizeChange(number) {
+    setPageAmount(number);
+  }
+
   return (
     <>
       <Center>
-        <Box m={"auto"} fontSize={"xl"}>
-          종합 게시판
-        </Box>
+        <Flex maxW={"500"}>
+          <Box m={"auto"} fontSize={"xl"}>
+            종합 게시판
+          </Box>
+          <Box m={"50"}>
+            <Menu textAlign={"center"} fontSize={"xl"}>
+              {({ isOpen }) => (
+                <>
+                  <MenuButton
+                    as={Button}
+                    rightIcon={
+                      isOpen ? (
+                        <span>
+                          <ChevronDownIcon />
+                        </span>
+                      ) : (
+                        <span>
+                          <ChevronUpIcon />
+                        </span>
+                      )
+                    }
+                    colorScheme={"blue"}
+                    size={"sm"}
+                  >
+                    {`게시글 (${pageAmount})개씩 보기`}
+                  </MenuButton>
+                  <MenuList>
+                    <MenuItem onClick={() => handlePageSizeChange(30)}>
+                      30개씩 보기
+                    </MenuItem>
+                    <MenuItem onClick={() => handlePageSizeChange(50)}>
+                      50개씩 보기
+                    </MenuItem>
+                    <MenuItem onClick={() => handlePageSizeChange(100)}>
+                      100개씩 보기
+                    </MenuItem>
+                  </MenuList>
+                </>
+              )}
+            </Menu>
+          </Box>
+        </Flex>
       </Center>
+
       <Center>
         <Box mb={10}></Box>
         <Box mb={10}>
