1730e2b5872d1b88404a659c4ecc145e5a0f457c
Dongwon <kdw686968@gmail.com>
2024-06-11 10:20:15 +0900

    backend: PlaceController 생성, domain 내에 hospital 생성

---
+++ backend/src/main/java/com/backend/controller/place/PlaceController.java
@@ -0,0 +1,11 @@
+package com.backend.controller.place;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/place")
+@RequiredArgsConstructor
+public class PlaceController {
+}
---
+++ backend/src/main/java/com/backend/domain/place/Hospital.java
@@ -0,0 +1,12 @@
+package com.backend.domain.place;
+
+import lombok.Data;
+
+@Data
+public class Hospital {
+    private int id;
+    private String name;
+    private String address;
+    private String callNumber;
+
+}
