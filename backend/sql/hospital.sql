d09ef8387e19cbb39a388ff1b26abf5a0c487857
Dongwon <kdw686968@gmail.com>
2024-06-11 15:30:48 +0900

    backend: hospital_comment 생성(리뷰 댓글 저장용)

--- backend/sql/hospital.sql
+++ backend/sql/hospital.sql
@@ -12,3 +12,15 @@ CREATE TABLE hospital
 
 INSERT INTO hospital(name, address, call_number)
 VALUES ('서울 성윤모 병원', '서울특별시 성북구 이대로 111', '02-1111-1111');
+
+# 댓글 테이블
+CREATE TABLE hospital_comment
+(
+    id        INT PRIMARY KEY AUTO_INCREMENT,
+    board_id  INT          NOT NULL REFERENCES hospital (hospital_id),
+    member_id INT          NOT NULL REFERENCES member (id),
+    comment   VARCHAR(500) NOT NULL,
+    inserted  DATETIME     NOT NULL DEFAULT NOW()
+);
+SELECT *
+FROM hospital_comment;
