69674469226b9a0f4bd9c32025d99004f1d803b8
Dongwon <kdw686968@gmail.com>
2024-06-12 11:11:26 +0900

    front: 댓글 전송 후 toast, 댓글 초기화

--- frontend/src/component/place/CommentWrite.jsx
+++ frontend/src/component/place/CommentWrite.jsx
@@ -1,4 +1,4 @@
-import { Box, Button, Textarea } from "@chakra-ui/react";
+import { Box, Button, Textarea, useToast } from "@chakra-ui/react";
 import { useState } from "react";
 import axios from "axios";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
@@ -6,7 +6,7 @@ import { faPaperPlane } from "@fortawesome/free-solid-svg-icons";
 
 export function CommentWrite({ hospitalId, isSending, setIsSending }) {
   const [comment, setComment] = useState("");
-
+  const toast = useToast();
   function handleCommentSubmitClick() {
     setIsSending(true);
     axios
@@ -14,7 +14,14 @@ export function CommentWrite({ hospitalId, isSending, setIsSending }) {
         hospitalId,
         comment,
       })
-      .then((res) => {})
+      .then((res) => {
+        setComment("");
+        toast({
+          description: "댓글이 등록되었습니다.",
+          position: "top",
+          status: "success",
+        });
+      })
       .catch()
       .finally(() => {
         setIsSending(false);
