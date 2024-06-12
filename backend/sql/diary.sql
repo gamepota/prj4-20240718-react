ac9defdbe41545297729d8219a3beeb56650f4cd
parkjaewook1 <wodnr5050@gmail.com>
2024-06-12 11:07:22 +0900

    back: 테스트 테이블 하나 만들어서 값 주고 난 뒤 반응 확인

--- backend/sql/diary.sql
+++ backend/sql/diary.sql
@@ -1,5 +1,21 @@
 CREATE TABLE diary
 (
+    id        INT PRIMARY KEY AUTO_INCREMENT,
+    title     VARCHAR(100)  NOT NULL,
+    content   VARCHAR(1000) NOT NULL,
+    writer    VARCHAR(100)  NOT NULL,
+    inserted  DATETIME      NOT NULL DEFAULT NOW(),
+    memberId  INT           NOT NULL REFERENCES member (id),
+    nick_name VARCHAR(255)  NOT NULL REFERENCES member (nickname)
+);
+
+DROP TABLE diary;
+
+SELECT *
+FROM diary;
+
+CREATE TABLE test
+(
     id       INT PRIMARY KEY AUTO_INCREMENT,
     title    VARCHAR(100)  NOT NULL,
     content  VARCHAR(1000) NOT NULL,
@@ -7,5 +23,8 @@ CREATE TABLE diary
     inserted DATETIME      NOT NULL DEFAULT NOW()
 );
 
+INSERT INTO test(id, title, content, writer)
+    VALUE ('1', '1', '1', '1');
+
 SELECT *
-FROM diary;
+FROM test;
