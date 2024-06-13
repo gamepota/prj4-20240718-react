bb2402ab5b2ab4eabbcb4161c38d375ea8b8ce3f
chaiheung <chaiheung@naver.com>
2024-06-13 14:34:34 +0900

    back: MemberLogin 기본 로직 작성

--- backend/src/main/java/com/backend/controller/member/MemberController.java
+++ backend/src/main/java/com/backend/controller/member/MemberController.java
@@ -3,10 +3,12 @@ package com.backend.controller.member;
 import com.backend.domain.member.Member;
 import com.backend.service.member.MemberService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequiredArgsConstructor
@@ -44,5 +46,17 @@ public class MemberController {
     @GetMapping("/list")
     public List<Member> list() {
         return service.list();
+    }
+
+    // MemberLogin
+    @PostMapping("token")
+    public ResponseEntity token(@RequestBody Member member) {
+        Map<String, Object> map = service.getToken(member);
+
+        if (map == null) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        }
+
+        return ResponseEntity.ok(map);
     }
 }
--- backend/src/main/java/com/backend/service/member/MemberService.java
+++ backend/src/main/java/com/backend/service/member/MemberService.java
@@ -5,15 +5,22 @@ import com.backend.domain.member.Role;
 import com.backend.mapper.member.MemberMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.oauth2.jwt.JwtClaimsSet;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.JwtEncoderParameters;
 import org.springframework.stereotype.Service;
 
+import java.time.Instant;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Service
 @RequiredArgsConstructor
 public class MemberService {
     private final MemberMapper mapper;
     private final BCryptPasswordEncoder passwordEncoder;
+    private final JwtEncoder jwtEncoder;
 
     //MemberSignup
     public void signup(Member member) {
@@ -33,5 +40,36 @@ public class MemberService {
     // MemberList
     public List<Member> list() {
         return mapper.selectAll();
+    }
+
+    // MemberLogin
+    public Map<String, Object> getToken(Member member) {
+
+        Map<String, Object> result = null;
+
+        Member db = mapper.selectByEmail(member.getEmail());
+
+        if (db != null) {
+            if (passwordEncoder.matches(member.getPassword(), db.getPassword())) {
+                result = new HashMap<>();
+                String token = "";
+                Instant now = Instant.now();
+                JwtClaimsSet claims = JwtClaimsSet.builder()
+                        .issuer("self")
+                        .issuedAt(now)
+                        .expiresAt(now.plusSeconds(60 * 60 * 24 * 7))
+                        .subject(member.getEmail())
+                        .claim("scope", "") // 권한
+                        .claim("nickname", db.getNickname())
+                        .build();
+
+                token = jwtEncoder.encode(JwtEncoderParameters.from(claims)).getTokenValue();
+
+                result.put("token", token);
+                result.put("token", db.getId());
+            }
+        }
+
+        return result;
     }
 }
