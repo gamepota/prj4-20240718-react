ec55f2a0b931ff4560940acb0fb0e635f72e303a
chaiheung <chaiheung@naver.com>
2024-06-16 23:53:46 +0900

    back: LoginFilter jwt 로직 작성중

--- backend/src/main/java/com/backend/jwt/LoginFilter.java
+++ backend/src/main/java/com/backend/jwt/LoginFilter.java
@@ -1,5 +1,44 @@
 package com.backend.jwt;
 
-public class LoginFilter {
-    
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+public class LoginFilter extends UsernamePasswordAuthenticationFilter {
+    private final AuthenticationManager authenticationManager;
+
+    public LoginFilter(AuthenticationManager authenticationManager) {
+        this.authenticationManager = authenticationManager;
+    }
+
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest req, HttpServletResponse res) throws AuthenticationException {
+
+        // 클라이언트 요청에서 email , password 추출
+        String email = obtainUsername(req);
+        String password = obtainPassword(req);
+
+        // token 에 담기
+        UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(email, password, null);
+
+        // AuthenticationManager 로 전달
+        return authenticationManager.authenticate(authToken);
+    }
+
+    // 로그인 성공시 실행 ( jwt 발급 )
+    @Override
+    protected void successfulAuthentication(HttpServletRequest req, HttpServletResponse res, FilterChain chain, Authentication authentication) {
+
+    }
+
+    // 로그인 실패시 실행
+    @Override
+    protected void unsuccessfulAuthentication(HttpServletRequest req, HttpServletResponse res, AuthenticationException failed) {
+
+    }
 }
