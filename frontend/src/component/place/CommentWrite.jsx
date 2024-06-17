7143d8b668484b060cb5b05a66166cc126fedab5
Dongwon <kdw686968@gmail.com>
2024-06-17 15:30:51 +0900

    front: 로그인 회우너만 리뷰 작성 가능

--- frontend/src/component/place/CommentWrite.jsx
+++ frontend/src/component/place/CommentWrite.jsx
@@ -1,13 +1,15 @@
-import { Box, Button, Textarea, useToast } from "@chakra-ui/react";
-import { useState } from "react";
+import { Box, Button, Textarea, Tooltip, useToast } from "@chakra-ui/react";
+import { useContext, useState } from "react";
 import axios from "axios";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPaperPlane } from "@fortawesome/free-solid-svg-icons";
+import { LoginContext } from "./LoginProvider.jsx";
 import { StarRating } from "./StarRating.jsx";
 
 export function CommentWrite({ hospitalId, isProcessing, setIsProcessing }) {
   const [comment, setComment] = useState("");
   const toast = useToast();
+  const account = useContext(LoginContext);
 
   function handleCommentSubmitClick() {
     setIsProcessing(true);
@@ -35,18 +37,29 @@ export function CommentWrite({ hospitalId, isProcessing, setIsProcessing }) {
     <Box>
       <StarRating />
       <Textarea
-        placeholder="리뷰를 남겨주세요."
+        isDisabled={!account.isLoggedIn()}
+        placeholder={
+          account.isLoggedIn()
+            ? "리뷰를 작성해 보세요."
+            : "리뷰를 작성하시려면 로그인하세요."
+        }
         value={comment}
         onChange={(e) => setComment(e.target.value)}
       />
-      <Button
-        isDisabled={comment.trim().length === 0}
-        isLoading={isProcessing}
-        onClick={handleCommentSubmitClick}
-        colorScheme="blue"
+      <Tooltip
+        label="로그인 하세요"
+        isDisabled={account.isLoggedIn()}
+        placement="top"
       >
-        <FontAwesomeIcon icon={faPaperPlane} />
-      </Button>
+        <Button
+          isDisabled={comment.trim().length === 0}
+          isLoading={isProcessing}
+          onClick={handleCommentSubmitClick}
+          colorScheme="blue"
+        >
+          <FontAwesomeIcon icon={faPaperPlane} />
+        </Button>
+      </Tooltip>
     </Box>
   );
 }
