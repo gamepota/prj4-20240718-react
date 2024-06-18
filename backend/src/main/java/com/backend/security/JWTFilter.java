703b9324c47d9ed270a53dc54ed9d9106235b75c
chaiheung <chaiheung@naver.com>
2024-06-18 23:30:30 +0900

    back: JWTFilter 로직 작성

--- backend/src/main/java/com/backend/security/JWTFilter.java
+++ backend/src/main/java/com/backend/security/JWTFilter.java
@@ -1,17 +1,75 @@
 package com.backend.security;
 
+import com.backend.domain.member.Member;
+import com.backend.domain.member.Role;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 
 public class JWTFilter extends OncePerRequestFilter {
 
+    private final JWTUtil jwtUtil;
+
+    public JWTFilter(JWTUtil jwtUtil) {
+        this.jwtUtil = jwtUtil;
+    }
+
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-        
+
+        // request 에서 Authorization 헤더 탐색
+        String authorization = request.getHeader("Authorization");
+
+        // Authorization 헤더 검증
+        if (authorization == null || !authorization.startsWith("Bearer ")) {
+
+            System.out.println("token null");
+            filterChain.doFilter(request, response);
+
+            // 조건이 해당되면 메소드 종료
+            return;
+        }
+
+        System.out.println("authorization now");
+        // 토큰만 획득
+        String token = authorization.split(" ")[1];
+
+        // 토큰 소멸 시간 검증
+        if (jwtUtil.isExpired(token)) {
+
+            System.out.println("token expired");
+            filterChain.doFilter(request, response);
+
+            // 조건이 해당되면 메소드 종료
+            return;
+        }
+
+        // 토큰에서 username 과 role 획득
+        String username = jwtUtil.getUsername(token);
+        String role = jwtUtil.getRole(token);
+
+        // member 에 값 set
+        Member member = new Member();
+        member.setUsername(username);
+        member.setPassword("password");
+        member.setRole(Role.USER);
+
+        // UserDetails 에 회원 정보 객체 담기
+        CustomUserDetails customUserDetails = new CustomUserDetails(member);
+
+        // spring security 인증 토큰 생성
+        Authentication authToken = new UsernamePasswordAuthenticationToken(customUserDetails, null, customUserDetails.getAuthorities());
+
+        // 세션에 사용자 등록
+        SecurityContextHolder.getContext().setAuthentication(authToken);
+
+        filterChain.doFilter(request, response);
     }
 }
