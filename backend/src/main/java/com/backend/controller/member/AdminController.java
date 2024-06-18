3c70a5849ae2ee6d08042e5310248778dbbd3d82
chaiheung <chaiheung@naver.com>
2024-06-19 02:13:42 +0900

    back: 테스트용 AdminController 생성

--- backend/src/main/java/com/backend/controller/member/AdminController.java
+++ backend/src/main/java/com/backend/controller/member/AdminController.java
@@ -1,4 +1,13 @@
 package com.backend.controller.member;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class AdminController {
+
+    @GetMapping("/admin")
+    public String admin() {
+        return "admin admin admin";
+    }
 }
