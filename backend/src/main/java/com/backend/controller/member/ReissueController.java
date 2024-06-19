abbc8a8fd54286ec7853e1bc28b12f54ec26bde5
chaiheung <chaiheung@naver.com>
2024-06-19 09:26:58 +0900

    back: refreshToken rotate

--- backend/src/main/java/com/backend/controller/member/ReissueController.java
+++ backend/src/main/java/com/backend/controller/member/ReissueController.java
@@ -8,9 +8,11 @@ import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api/member")
 public class ReissueController {
 
     private final JWTUtil jwtUtil;
@@ -63,10 +65,23 @@ public class ReissueController {
 
         // make new JWT
         String newAccess = jwtUtil.createJwt("access", username, role, 600000L);
+        String newRefresh = jwtUtil.createJwt("refresh", username, role, 86400000L);
 
         // response
         response.setHeader("access", newAccess);
+        response.addCookie(createCookie("refresh", newRefresh));
 
         return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    private Cookie createCookie(String key, String value) {
+
+        Cookie cookie = new Cookie(key, value);
+        cookie.setMaxAge(24 * 60 * 60);
+        //cookie.setSecure(true);
+        //cookie.setPath("/");
+        cookie.setHttpOnly(true);
+
+        return cookie;
     }
 }
