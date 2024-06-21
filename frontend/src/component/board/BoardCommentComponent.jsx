47f8a40f05831e77b474108de966d30751a40c00
ganghyeon jo <ganghyeon4479@naver.com>
2024-06-21 14:48:48 +0900

    frontend --App에 loginprovider 제공 안한 코드2

--- frontend/src/component/board/BoardCommentComponent.jsx
+++ frontend/src/component/board/BoardCommentComponent.jsx
@@ -1,13 +1,7 @@
 import React, { useState } from "react";
 import { Box } from "@chakra-ui/react";
 import { BoardCommentWrite } from "./BoardCommentWrite.jsx";
-import * as PropTypes from "prop-types";
 import { BoardCommentList } from "./BoardCommentList.jsx";
-
-BoardCommentList.propTypes = {
-  isProcessing: PropTypes.bool,
-  setIsProcessing: PropTypes.func,
-};
 
 export function BoardCommentComponent({ boardId }) {
   const [isProcessing, setIsProcessing] = useState(false);
