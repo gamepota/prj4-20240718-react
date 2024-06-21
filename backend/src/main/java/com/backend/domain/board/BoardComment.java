45ec1fa464e2cf1952a1fd8b6529498cc7031ead
ganghyeon jo <ganghyeon4479@naver.com>
2024-06-21 12:16:48 +0900

    backend -- boardcomment 객체 생성

---
+++ backend/src/main/java/com/backend/domain/board/BoardComment.java
@@ -0,0 +1,27 @@
+package com.backend.domain.board;
+
+import lombok.Data;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+@Data
+public class BoardComment {
+    private Integer id;
+    private Integer boardId;
+    private Integer memberId;
+    private String comment;
+    private LocalDateTime inserted;
+
+    public String getInserted() {
+        LocalDateTime beforeOneDay = LocalDateTime.now().minusDays(1);
+
+        if (inserted.isBefore(beforeOneDay)) {
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+            return inserted.format(formatter).toString();
+        } else {
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm:ss");
+            return inserted.format(formatter).toString();
+        }
+    }
+}
