b8865666bd5318173727b66040710c455c981dfa
Dongwon <kdw686968@gmail.com>
2024-06-24 09:29:33 +0900

    front: kakaoMap Component 기본 지도 생성

--- frontend/src/KakaoMap.jsx
+++ frontend/src/KakaoMap.jsx
@@ -1,105 +1,32 @@
 // src/components/KakaoMap.jsx
 
-import React, { useEffect, useState } from "react";
-import { Box } from "@chakra-ui/react";
-import axios from "axios";
+import React, { useEffect, useRef, useState } from "react";
 
 const KakaoMap = () => {
-  const [geojson, setGeojson] = useState(null);
+  const kakaoMaps = useRef(null);
+  const [map, setMap] = useState(null);
 
+  const getKakao = () => {
+    const mapContainer = document.getElementById("map");
+    if (!map) {
+      const mapOptions = {
+        center: new window.kakao.maps.LatLng(33.450701, 126.570667),
+        level: 4,
+      };
+      const map = new window.kakao.maps.Map(mapContainer, mapOptions);
+      setMap(map);
+    }
+  };
   useEffect(() => {
-    const fetchGeojson = async () => {
-      try {
-        const response = await axios.get("/data/json/geojson.json");
-        setGeojson(response.data);
-      } catch (error) {
-        console.error("Error fetching geojson:", error);
-      }
-    };
-
-    fetchGeojson();
+    getKakao();
   }, []);
-
-  useEffect(() => {
-    // if (!geojson) {
-    //   return null; // 또는 로딩 스피너를 표시할 수 있습니다.
-    // }
-    // if (geojson.isEmpty(false)) return;
-
-    const script = document.createElement("script");
-    script.src = `//dapi.kakao.com/v2/maps/sdk.js?appkey=d5b3cb3d230c4f406001bbfad60ef4d4&libraries=services,clusterer,drawing`;
-    script.async = true;
-    document.head.appendChild(script);
-
-    script.onload = () => {
-      const kakao = window.kakao;
-      const data = geojson.features;
-
-      const mapContainer = document.getElementById("pollution-map");
-      const mapOption = {
-        center: new kakao.maps.LatLng(37.566826, 126.9786567),
-        level: 9,
-      };
-
-      const map = new kakao.maps.Map(mapContainer, mapOption);
-      const customOverlay = new kakao.maps.CustomOverlay({});
-      new kakao.maps.InfoWindow({ removable: true });
-      const displayArea = (coordinates, name) => {
-        const path = [];
-
-        coordinates[0].forEach((coordinate) => {
-          path.push(new kakao.maps.LatLng(coordinate[1], coordinate[0]));
-        });
-
-        const polygon = new kakao.maps.Polygon({
-          map: map,
-          path: path,
-          strokeWeight: 2,
-          strokeColor: "#004c80",
-          strokeOpacity: 0.8,
-          strokeStyle: "solid",
-          fillColor: "#fff",
-          fillOpacity: 0.7,
-        });
-
-        kakao.maps.event.addListener(
-          polygon,
-          "mouseover",
-          function (mouseEvent) {
-            polygon.setOptions({ fillColor: "#09f" });
-            customOverlay.setContent('<div class="area">' + name + "</div>");
-            customOverlay.setPosition(mouseEvent.latLng);
-            customOverlay.setMap(map);
-          },
-        );
-
-        kakao.maps.event.addListener(
-          polygon,
-          "mousemove",
-          function (mouseEvent) {
-            customOverlay.setPosition(mouseEvent.latLng);
-          },
-        );
-
-        kakao.maps.event.addListener(polygon, "mouseout", function () {
-          polygon.setOptions({ fillColor: "#fff" });
-          customOverlay.setMap(null);
-        });
-      };
-
-      data.forEach((val) => {
-        const coordinates = val.geometry.coordinates;
-        const name = val.properties.SIG_KOR_NM;
-        displayArea(coordinates, name);
-      });
-    };
-  }, [geojson]);
-
   return (
-    <Box id="pollution-map" style={{ width: "100%", height: "500px" }}></Box>
+    <div
+      id="map"
+      ref={kakaoMaps}
+      style={{ width: "100vw", height: "100vh" }}
+    ></div>
   );
 };
 
 export default KakaoMap;
-
-//d5b3cb3d230c4f406001bbfad60ef4d4
