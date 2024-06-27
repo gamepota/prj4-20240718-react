f2a044bd7d827486d8b40788366f4ec381a29801
ganghyeon jo <ganghyeon4479@naver.com>
2024-06-27 18:10:11 +0900

    front  --댓글 작성 및 보기

--- frontend/src/component/board/BoardCommentComponent.jsx
+++ frontend/src/component/board/BoardCommentComponent.jsx
@@ -1,5 +1,5 @@
 import React, { useContext, useState } from "react";
-import { Box, Button } from "@chakra-ui/react";
+import { Box, Button, Tooltip } from "@chakra-ui/react";
 import { BoardCommentWrite } from "./BoardCommentWrite.jsx";
 import { BoardCommentList } from "./BoardCommentList.jsx";
 import { LoginContext } from "../LoginProvider.jsx";
@@ -7,26 +7,40 @@ import { LoginContext } from "../LoginProvider.jsx";
 export function BoardCommentComponent({ boardId }) {
   const [isProcessing, setIsProcessing] = useState(false);
   const [showCommentWrite, setShowCommentWrite] = useState(false);
-  const { memberInfo, setMemberInfo } = useContext(LoginContext);
+  const { memberInfo } = useContext(LoginContext);
   const memberId = memberInfo && memberInfo.id ? parseInt(memberInfo.id) : null;
-  const params = memberId ? { memberId } : {};
 
   const handleCommentButtonClick = () => {
-    setShowCommentWrite(!showCommentWrite);
+    if (memberInfo) {
+      setShowCommentWrite(!showCommentWrite);
+    }
   };
 
   return (
     <Box>
-      <Button onClick={handleCommentButtonClick} mb={4} colorScheme="blue">
-        {showCommentWrite ? "댓글 작성 취소" : "댓글 작성"}
-      </Button>
+      <Tooltip
+        isDisabled={!!memberInfo}
+        hasArrow
+        label="로그인 해주세요."
+        placement="top"
+      >
+        <Button
+          onClick={handleCommentButtonClick}
+          mb={4}
+          colorScheme="blue"
+          isDisabled={!memberInfo}
+        >
+          {showCommentWrite ? "댓글 작성 취소" : "댓글 작성"}
+        </Button>
+      </Tooltip>
 
-      {showCommentWrite && (
+      {showCommentWrite && memberInfo && (
         <BoardCommentWrite
           boardId={boardId}
           isProcessing={isProcessing}
           setIsProcessing={setIsProcessing}
           onCommentSubmit={() => setShowCommentWrite(false)}
+          memberId={memberId}
         />
       )}
 
