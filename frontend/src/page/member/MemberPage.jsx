7fbc6c46dddd6ff424a61ca2977ff676325774cf
chaiheung <chaiheung@naver.com>
2024-06-30 04:26:35 +0900

    front: 정보 출력 오류 수정

--- frontend/src/page/member/MemberPage.jsx
+++ frontend/src/page/member/MemberPage.jsx
@@ -46,6 +46,7 @@ export function MemberPage() {
           setHasProfileImage(true);
         }
       } catch (err) {
+        console.error(err);
         Swal.fire({
           title: "회원 정보를 불러오는 데 실패했습니다.",
           text: "오류가 발생하였습니다. 잠시 후 다시 시도해주세요.",
