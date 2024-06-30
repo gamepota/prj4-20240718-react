f3971fdfd1797d16223fb4bdcf426cda9b6c6889
parkjaewook1 <wodnr5050@gmail.com>
2024-06-30 23:46:05 +0900

    front : DiaryCommentList 에서 안쓰는 클릭들 삭제

--- frontend/src/page/diary/diarySrc/diaryPage/diaryComment/DiaryCommentList.jsx
+++ frontend/src/page/diary/diarySrc/diaryPage/diaryComment/DiaryCommentList.jsx
@@ -21,73 +21,71 @@ export function DiaryCommentList({ diaryCommentList }) {
   const { memberInfo } = useContext(LoginContext);
   const navigate = useNavigate();
   const dateFnsDate = new Date();
-  function handleWriteClick() {
-    const diaryId = generateDiaryId(memberInfo.id);
-    navigate(`/diary/${diaryId}/comment/write/${diaryId}`);
-  }
-  function handleViewClick(commentId) {
-    const diaryId = generateDiaryId(memberInfo.id);
-    return () => navigate(`/diary/${diaryId}/comment/view/${commentId}`);
-  }
+
   function handleFriendsHome() {
     const diaryId = generateDiaryId(memberInfo.id);
     navigate(`/diary/${diaryId}`);
   }
+
   return (
-      <Box m={4}>
-        {" "}
-        {/* 전체 리스트의 위아래 공간을 추가 */}
-        <Box mb={20}></Box>
-        <Box>
-          {diaryCommentList.length === 0 && <Center>방명록이 없습니다.</Center>}
-          {diaryCommentList.length > 0 && (
-              <VStack spacing={3} align="stretch">
-                {diaryCommentList.map((diaryComment) => (
-                    <Box
-                        key={diaryComment.id}
-                        p={4} // 아이템의 패딩을 추가하여 내부 공간을 확보
-                        borderWidth="10px"
-                        borderRadius="md"
-                        // onClick={handleViewClick(diaryComment.id)}
-                    >
-                      <HStack spacing={3}>
-                        <Image
-                            src={diaryComment.photoUrl}
-                            boxSize="200px"
-                            borderRadius="full"
-                        />
-                        <Flex direction="column" align="flex-end" w="100%">
-                          <HStack w="100%" justify="space-between" spacing={2}>
-                            <HStack spacing={1}>
-                              <Text fontSize="13px">No.{diaryComment.id}</Text>
-                              <Text fontWeight="bold">{memberInfo.nickname}</Text>
-                              <Button
-                                  onClick={handleFriendsHome}
-                                  p={0}
-                                  m={0}
-                                  bg="transparent"
-                                  _hover={{ bg: "transparent" }}
-                              >
-                                <FontAwesomeIcon icon={faHouseChimney} />
-                              </Button>
-                            </HStack>
-                            <Text fontSize="sm" color="gray.500">
-                              <span style={{ color: "red" }} />
-                              {format(dateFnsDate, "yyyy.MM.dd")}
-                            </Text>
-                          </HStack>
-                          <Box w="100%" textAlign="center" mt={2}>
-                            <Textarea readOnly size="sm" resize="none" p={1}>
-                              {diaryComment.comment}
-                            </Textarea>
-                          </Box>
-                        </Flex>
+    <Box m={4}>
+      {" "}
+      {/* 전체 리스트의 위아래 공간을 추가 */}
+      <Box mb={20}></Box>
+      <Box>
+        {diaryCommentList.length === 0 && <Center>방명록이 없습니다.</Center>}
+        {diaryCommentList.length > 0 && (
+          <VStack spacing={3} align="stretch">
+            {diaryCommentList.map((diaryComment) => (
+              <Box
+                key={diaryComment.id}
+                p={4} // 아이템의 패딩을 추가하여 내부 공간을 확보
+                borderWidth="10px"
+                borderRadius="md"
+                // onClick={handleViewClick(diaryComment.id)}
+              >
+                <HStack spacing={3}>
+                  <Image
+                    src={diaryComment.photoUrl}
+                    boxSize="200px"
+                    borderRadius="full"
+                  />
+                  <Flex direction="column" align="flex-end" w="100%">
+                    <HStack w="100%" justify="space-between" spacing={2}>
+                      <HStack spacing={1}>
+                        <Text fontSize="13px">No.{diaryComment.id}</Text>
+                        <Text fontWeight="bold">{memberInfo.nickname}</Text>
+                        <Button
+                          onClick={handleFriendsHome}
+                          p={0}
+                          m={0}
+                          bg="transparent"
+                          _hover={{ bg: "transparent" }}
+                        >
+                          <FontAwesomeIcon icon={faHouseChimney} />
+                        </Button>
                       </HStack>
+                      <Text fontSize="sm" color="gray.500">
+                        <span style={{ color: "red" }} />
+                        {format(dateFnsDate, "yyyy.MM.dd")}
+                      </Text>
+                    </HStack>
+                    <Box w="100%" textAlign="center" mt={2}>
+                      <Textarea
+                        value={diaryComment.comment}
+                        readOnly
+                        size="sm"
+                        resize="none"
+                        p={1}
+                      />
                     </Box>
-                ))}
-              </VStack>
-          )}
-        </Box>
+                  </Flex>
+                </HStack>
+              </Box>
+            ))}
+          </VStack>
+        )}
       </Box>
+    </Box>
   );
 }
