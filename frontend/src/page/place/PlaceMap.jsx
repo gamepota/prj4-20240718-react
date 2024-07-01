9ec9c7bbf5b9eb3599859dcff01524ce4011d2e9
Dongwon <kdw686968@gmail.com>
2024-07-01 12:15:17 +0900

    front: 기본 지도 설정 완료. 검색 창 기능 삽입 해야함

--- frontend/src/page/place/PlaceMap.jsx
+++ frontend/src/page/place/PlaceMap.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import KakaoMap from "../../KakaoMap.jsx";
+import { Box } from "@chakra-ui/react";
 import { PlaceMap2 } from "./PlaceMap2.jsx";
 
 export function PlaceMap() {
@@ -11,8 +12,28 @@ export function PlaceMap() {
 
   return (
     <div className="App">
-      <KakaoMap onPolygonSelect={handlePolygonSelect} />
-      {selectedCtprvnCd && <PlaceMap2 ctprvnCd={selectedCtprvnCd} />}
+      <Box position="relative" width="100%" height="500px">
+        <Box
+          position="absolute"
+          top="0"
+          left="0"
+          width="100%"
+          height="100%"
+          display={selectedCtprvnCd ? "none" : "block"}
+        >
+          <KakaoMap onPolygonSelect={handlePolygonSelect} />
+        </Box>
+        <Box
+          position="absolute"
+          top="0"
+          left="0"
+          width="100%"
+          height="100%"
+          display={selectedCtprvnCd ? "block" : "none"}
+        >
+          <PlaceMap2 ctprvnCd={selectedCtprvnCd} />
+        </Box>
+      </Box>
     </div>
   );
 }
