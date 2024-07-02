5a00c8824c866c681644f84fd7255aed3cb09412
ganghyeon jo <ganghyeon4479@naver.com>
2024-07-02 19:10:27 +0900

    front,back  --boardview 프로필 이미지추가...

--- frontend/src/page/board/BoardView.jsx
+++ frontend/src/page/board/BoardView.jsx
@@ -1,4 +1,5 @@
 import {
+  Avatar,
   Badge,
   Box,
   Button,
@@ -37,6 +38,8 @@ export function BoardView() {
     count: 0,
   });
   const [isLikeProcessing, setIsLikeProcessing] = useState(false);
+  const [profileImage, setProfileImage] = useState(null);
+
   const {
     isOpen: isOpenDelete,
     onOpen: onOpenDelete,
@@ -62,6 +65,7 @@ export function BoardView() {
       .then((res) => {
         setBoard(res.data.board);
         setLike(res.data.like);
+        fetchProfileImage(res.data.board.memberId);
       })
       .catch((err) => {
         if (err.response.status === 404) {
@@ -75,180 +79,208 @@ export function BoardView() {
       });
   }, [id]);
 
-  if (board === null) {
-    return <Spinner />;
-  }
-
-  const handleClickRemove = () => {
-    axios
-      .delete(`/api/board/${board.id}`, { params })
-      .then(() => {
-        toast({
-          status: "success",
-          description: `${id}번 게시물이 삭제되었습니다`,
-          position: "top",
-          duration: 10,
-        });
-        navigate(`/`);
-      })
-      .catch(() => {
-        toast({
-          status: "error",
-          description: "잘못된 삭제 명령입니다",
-          position: "top",
-          duration: 10,
-        });
-      })
-      .finally(onCloseDelete);
-  };
-
-  const handleClickLike = () => {
-    if (!memberInfo) {
-      return;
+  async function fetchProfileImage(memberId) {
+    try {
+      const res = await axios.get(`/api/member/${memberId}`);
+      setProfileImage(res.data.imageUrl);
+      console.log("Fetched profileImage=", res.data.imageUrl);
+    } catch (error) {
+      console.error("Error fetching profile image:", error);
     }
-    setIsLikeProcessing(true);
-    axios
-      .put("/api/board/like", { boardId: board.id, memberId: memberInfo.id })
-      .then((res) => {
-        setLike(res.data);
-      })
-      .catch((err) => {
-        console.log(err);
-      })
-      .finally(() => {
-        setIsLikeProcessing(false);
-      });
-  };
 
-  const handleWriterClick = (writer, writerId) => {
-    setSelectedWriter(writer);
-    setSelectedWriterId(writerId);
-  };
+    if (board === null) {
+      return <Spinner />;
+    }
 
-  const handleDiaryView = () => {
-    const diaryId = generateDiaryId(selectedWriterId);
-    const url = `/diary/${diaryId}`;
-    const windowFeatures = "width=1531,height=864";
-    window.open(url, "_blank", windowFeatures);
-  };
+    const handleClickRemove = () => {
+      axios
+        .delete(`/api/board/${board.id}`, { params })
+        .then(() => {
+          toast({
+            status: "success",
+            description: `${id}번 게시물이 삭제되었습니다`,
+            position: "top",
+            duration: 10,
+          });
+          navigate(`/`);
+        })
+        .catch(() => {
+          toast({
+            status: "error",
+            description: "잘못된 삭제 명령입니다",
+            position: "top",
+            duration: 10,
+          });
+        })
+        .finally(onCloseDelete);
+    };
 
-  return (
-    <Container maxW="container.xl" py={10}>
-      <Box p={6} borderWidth="1px" borderRadius="md" bg="white" mb={6}>
-        <Flex justify="space-between" align="center" mb={4}>
-          <Box fontWeight="bold" fontSize="2xl">
-            {board.title}
-          </Box>
-          <Flex align="center">
-            <Box fontSize="sm" color="gray.600" mr={2}>
-              {new Date(board.inserted).toLocaleString()}
+    const handleClickLike = () => {
+      if (!memberInfo) {
+        return;
+      }
+      setIsLikeProcessing(true);
+      axios
+        .put("/api/board/like", { boardId: board.id, memberId: memberInfo.id })
+        .then((res) => {
+          setLike(res.data);
+        })
+        .catch((err) => {
+          console.log(err);
+        })
+        .finally(() => {
+          setIsLikeProcessing(false);
+        });
+    };
+
+    const handleWriterClick = (writer, writerId) => {
+      setSelectedWriter(writer);
+      setSelectedWriterId(writerId);
+    };
+
+    const handleDiaryView = () => {
+      const diaryId = generateDiaryId(selectedWriterId);
+      const url = `/diary/${diaryId}`;
+      const windowFeatures = "width=1531,height=864";
+      window.open(url, "_blank", windowFeatures);
+    };
+
+    return (
+      <Container maxW="container.xl" py={10}>
+        <Box p={6} borderWidth="1px" borderRadius="md" bg="white" mb={6}>
+          <Flex justify="space-between" align="center" mb={4}>
+            <Box fontWeight="bold" fontSize="2xl">
+              {board.title}
             </Box>
-            <Popover>
-              <PopoverTrigger>
-                <Badge
-                  cursor="pointer"
-                  color="blue.600"
-                  onClick={() =>
-                    handleWriterClick(board.writer, board.memberId)
-                  }
-                >
-                  {board.writer}
-                </Badge>
-              </PopoverTrigger>
-              <PopoverContent>
-                <PopoverArrow />
-                <PopoverCloseButton />
-                <PopoverBody>
-                  <Button colorScheme="blue" onClick={handleDiaryView}>
-                    작성자 다이어리 보기
-                  </Button>
-                </PopoverBody>
-              </PopoverContent>
-            </Popover>
+            <Flex align="center">
+              <Box fontSize="sm" color="gray.600" mr={2}>
+                {new Date(board.inserted).toLocaleString()}
+              </Box>
+              <Popover>
+                <PopoverTrigger>
+                  {profileImage ? (
+                    <Image
+                      src={profileImage}
+                      boxSize="40px"
+                      borderRadius="full"
+                      mr={2}
+                      sx={{ cursor: "pointer" }}
+                      onClick={handleDiaryView}
+                    />
+                  ) : (
+                    <Avatar
+                      name={board.writer}
+                      size="sm"
+                      mr={2}
+                      sx={{ cursor: "pointer" }}
+                      onClick={handleDiaryView}
+                    />
+                  )}
+                  <Badge
+                    cursor="pointer"
+                    color="blue.600"
+                    onClick={() =>
+                      handleWriterClick(board.writer, board.memberId)
+                    }
+                  >
+                    {board.writer}
+                  </Badge>
+                </PopoverTrigger>
+                <PopoverContent>
+                  <PopoverArrow />
+                  <PopoverCloseButton />
+                  <PopoverBody>
+                    <Button colorScheme="blue" onClick={handleDiaryView}>
+                      작성자 다이어리 보기
+                    </Button>
+                  </PopoverBody>
+                </PopoverContent>
+              </Popover>
+            </Flex>
           </Flex>
-        </Flex>
-        <Flex justify="space-between" align="center">
-          <Box>추천수: {like.count}</Box>
-        </Flex>
-      </Box>
-      <Box p={6} borderWidth="1px" borderRadius="md" bg="white">
-        {board.fileList &&
-          board.fileList.map((file) => (
-            <Box key={file.name} mb={3}>
-              <Image src={file.src} alt={file.name} borderRadius="md" />
+          <Flex justify="space-between" align="center">
+            <Box>추천수: {like.count}</Box>
+          </Flex>
+        </Box>
+        <Box p={6} borderWidth="1px" borderRadius="md" bg="white">
+          {board.fileList &&
+            board.fileList.map((file) => (
+              <Box key={file.name} mb={3}>
+                <Image src={file.src} alt={file.name} borderRadius="md" />
+              </Box>
+            ))}
+          {board.content}
+        </Box>
+        <Flex mt={4} mb={4} align="center">
+          <Tooltip isDisabled={memberInfo} hasArrow label="로그인 해주세요.">
+            <Box onClick={handleClickLike} cursor="pointer" fontSize="3xl">
+              {like.like ? (
+                <FontAwesomeIcon icon={fullHeart} />
+              ) : (
+                <FontAwesomeIcon icon={emptyHeart} />
+              )}
             </Box>
-          ))}
-        {board.content}
-      </Box>
-      <Flex mt={4} mb={4} align="center">
-        <Tooltip isDisabled={memberInfo} hasArrow label="로그인 해주세요.">
-          <Box onClick={handleClickLike} cursor="pointer" fontSize="3xl">
-            {like.like ? (
-              <FontAwesomeIcon icon={fullHeart} />
-            ) : (
-              <FontAwesomeIcon icon={emptyHeart} />
-            )}
-          </Box>
-        </Tooltip>
-        {like.count > 0 && (
-          <Box mx={3} fontSize="3xl">
-            {like.count}
-          </Box>
-        )}
-        <Spacer />
-        <Button
-          onClick={() => {
-            if (!memberInfo) {
-              toast({
-                description: "로그인 해주시길 바랍니다",
-                duration: 5000,
-                position: "top",
-                isClosable: "true",
-              });
-            } else {
-              onOpenReport();
-            }
-          }}
-        >
-          신고
-        </Button>
-        {isLikeProcessing && (
-          <Box ml={2}>
-            <Spinner size="sm" />
-          </Box>
-        )}
-      </Flex>
-      <Box fontSize={"large"} color="gray.500">조회수: {board.views}</Box>
-      <Box mt={4}>
-        <BoardCommentComponent boardId={board.id} />
-      </Box>
-      {(memberId === board.memberId || memberId == 1) && (
-        <Flex justify="flex-end" mt={4}>
+          </Tooltip>
+          {like.count > 0 && (
+            <Box mx={3} fontSize="3xl">
+              {like.count}
+            </Box>
+          )}
+          <Spacer />
           <Button
-            colorScheme="purple"
-            onClick={() => navigate(`/board/edit/${id}`)}
-            mr={2}
+            onClick={() => {
+              if (!memberInfo) {
+                toast({
+                  description: "로그인 해주시길 바랍니다",
+                  duration: 5000,
+                  position: "top",
+                  isClosable: "true",
+                });
+              } else {
+                onOpenReport();
+              }
+            }}
           >
-            수정
+            신고
           </Button>
-          <Button colorScheme="red" onClick={onOpenDelete}>
-            삭제
-          </Button>
+          {isLikeProcessing && (
+            <Box ml={2}>
+              <Spinner size="sm" />
+            </Box>
+          )}
         </Flex>
-      )}
-      <DeleteConfirmationModal
-        isOpen={isOpenDelete}
-        onClose={onCloseDelete}
-        onClickRemove={handleClickRemove}
-      />
-      <ReportModal
-        isOpen={isOpenReport}
-        onClose={onCloseReport}
-        boardId={board.id}
-        memberId={params.memberId}
-      />
-    </Container>
-  );
+        <Box fontSize={"large"} color="gray.500">
+          조회수: {board.views}
+        </Box>
+        <Box mt={4}>
+          <BoardCommentComponent boardId={board.id} />
+        </Box>
+        {(memberId === board.memberId || memberId == 1) && (
+          <Flex justify="flex-end" mt={4}>
+            <Button
+              colorScheme="purple"
+              onClick={() => navigate(`/board/edit/${id}`)}
+              mr={2}
+            >
+              수정
+            </Button>
+            <Button colorScheme="red" onClick={onOpenDelete}>
+              삭제
+            </Button>
+          </Flex>
+        )}
+        <DeleteConfirmationModal
+          isOpen={isOpenDelete}
+          onClose={onCloseDelete}
+          onClickRemove={handleClickRemove}
+        />
+        <ReportModal
+          isOpen={isOpenReport}
+          onClose={onCloseReport}
+          boardId={board.id}
+          memberId={params.memberId}
+        />
+      </Container>
+    );
+  }
 }
-
-export default BoardView;
