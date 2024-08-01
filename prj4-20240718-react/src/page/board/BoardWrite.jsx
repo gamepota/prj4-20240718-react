f916235c90faccaab918c2d6c1a57dcba9350eb1
kdw9805 <kdw686968@gmail.com>
2024-08-01 10:15:38 +0900

    글작성 화면

--- prj4-20240718-react/src/page/board/BoardWrite.jsx
+++ prj4-20240718-react/src/page/board/BoardWrite.jsx
@@ -1,6 +1,55 @@
-import { Box } from "@chakra-ui/react";
-import React from "react";
+import {
+  Box,
+  Button,
+  FormControl,
+  FormLabel,
+  Input,
+  Textarea,
+} from "@chakra-ui/react";
+import React, { useState } from "react";
+import axios from "axios";
 
 export function BoardWrite() {
-  return <Box> 글 작성 화면</Box>;
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+  const [writer, setWriter] = useState("");
+
+  function handleSaveClick() {
+    axios.post("/api/board/add", {
+      title,
+      content,
+      writer,
+    });
+  }
+
+  return (
+    <Box>
+      <Box> 글 작성 화면</Box>
+      <Box>
+        <Box>
+          <FormControl>
+            <FormLabel>제목</FormLabel>
+            <Input onChange={(e) => setTitle(e.target.value)} />
+          </FormControl>
+        </Box>
+        <Box>
+          <FormControl>
+            <FormLabel>본문</FormLabel>
+            <Textarea onChange={(e) => setContent(e.target.value)} />
+          </FormControl>
+        </Box>
+        <Box>
+          <FormControl>
+            <FormLabel>작성자</FormLabel>
+            <Input onChange={(e) => setWriter(e.target.value)} />
+          </FormControl>
+        </Box>
+        <Box>
+          <Button colorScheme={"blue"} onClick={handleSaveClick}>
+            저장
+          </Button>
+        </Box>
+      </Box>
+    </Box>
+  );
 }
