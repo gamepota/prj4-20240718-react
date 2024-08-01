78f9aace642e2daa6edd4528a6aa388fb68d26fb
kdw9805 <kdw686968@gmail.com>
2024-08-01 14:50:52 +0900

    게시물 보기 화면 에서  get 메소드 추가

--- prj4-20240718-react/src/page/board/BoardList.jsx
+++ prj4-20240718-react/src/page/board/BoardList.jsx
@@ -1,10 +1,15 @@
 import { Box, Table, Tbody, Td, Th, Thead, Tr } from "@chakra-ui/react";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faUserPen } from "@fortawesome/free-solid-svg-icons";
+import axios from "axios";
 
 export function BoardList() {
   const [boardList, setBoardList] = useState([]);
+
+  useEffect(() => {
+    axios.get("/api/board/list").then((res) => setBoardList(res.data));
+  }, []);
   return (
     <Box>
       <Box>게시물 목록</Box>
