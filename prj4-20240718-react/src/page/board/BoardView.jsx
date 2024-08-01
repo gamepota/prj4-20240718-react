9efa7fd842f456db29f26274e12cd626fb662011
kdw9805 <kdw686968@gmail.com>
2024-08-01 16:37:56 +0900

    게시물 삭제 수정2

--- prj4-20240718-react/src/page/board/BoardView.jsx
+++ prj4-20240718-react/src/page/board/BoardView.jsx
@@ -1,5 +1,5 @@
 import { useNavigate, useParams } from "react-router-dom";
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import axios from "axios";
 import {
   Box,
