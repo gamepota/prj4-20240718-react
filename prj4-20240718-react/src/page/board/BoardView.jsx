9f8f0a8b90044bbc59b4abfee4a6e4afd763d88b
kdw9805 <kdw686968@gmail.com>
2024-08-01 16:18:29 +0900

    게시물 삭제(modal로 삭제 확인 기능, 삭제 확인 메세지 탑재)

--- prj4-20240718-react/src/page/board/BoardView.jsx
+++ prj4-20240718-react/src/page/board/BoardView.jsx
@@ -1,26 +1,74 @@
-import { useParams } from "react-router-dom";
-import { useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
 import {
   Box,
+  Button,
   FormControl,
   FormLabel,
   Input,
+  Modal,
+  ModalBody,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
   Spinner,
   Textarea,
+  useDisclosure,
+  useToast,
 } from "@chakra-ui/react";
 
 export function BoardView() {
   const { id } = useParams();
   const [board, setBoard] = useState(null);
+  const toast = useToast();
+  const navigate = useNavigate();
+  const { isOpen, onClose, onOpen } = useDisclosure();
 
   useEffect(() => {
-    axios.get(`/api/board/${id}`).then((res) => setBoard(res.data));
+    axios
+      .get(`/api/board/${id}`)
+      .then((res) => setBoard(res.data))
+      .catch((err) => {
+        if (err.response.status === 404) {
+          toast({
+            status: "info",
+            description: "해당 게시물이 존재하지 않습니다.",
+            position: "top",
+          });
+          navigate("/");
+        }
+      });
   }, []);
 
   if (board === null) {
     return <Spinner />;
   }
+
+  function handleClickRemove() {
+    axios
+      .delete(`/api/board/${id}`)
+      .then(() => {
+        toast({
+          status: "success",
+          description: `${id}번 게시물이 삭제되었습니다.`,
+          position: "top",
+        });
+        navigate("/");
+      })
+      .catch(() => {
+        toast({
+          status: "error",
+          description: `${id}번 게시물 삭제 중 오류가 발생하였습니다.`,
+          position: "top",
+        });
+      })
+      .finally(() => {
+        onClose();
+      });
+  }
+
   return (
     <Box>
       <Box>{board.id}번 게시물</Box>
@@ -46,6 +94,28 @@ export function BoardView() {
         <FormControl>작성일시</FormControl>
         <Input type={"datetime-local"} value={board.inserted} readOnly />
       </Box>
+      <Box>
+        <Button colorScheme={"red"} onClick={onOpen}>
+          삭제
+        </Button>
+        <Button colorScheme={"blue"} onClick={navigate()}>
+          수정
+        </Button>
+      </Box>
+
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader></ModalHeader>
+          <ModalBody>삭제하시겠습니까?</ModalBody>
+          <ModalFooter>
+            <Button onClick={onClose}>취소</Button>
+            <Button colorScheme={"red"} onClick={handleClickRemove}>
+              확인
+            </Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
     </Box>
   );
 }
