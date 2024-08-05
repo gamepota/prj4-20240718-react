2a6a154cc10c6560327420606a0b204864641f91
kdw9805 <kdw686968@gmail.com>
2024-08-05 09:31:43 +0900

    회원가입화면 가입 버튼 클릭시 post /api/member/signup 전송 then,catch,finally 작성 26열 수정

--- prj4-20240718-react/src/page/member/MemberSignup.jsx
+++ prj4-20240718-react/src/page/member/MemberSignup.jsx
@@ -1,3 +1,95 @@
+import {
+  Box,
+  Button,
+  FormControl,
+  FormLabel,
+  Input,
+  InputGroup,
+  InputRightElement,
+  useToast,
+} from "@chakra-ui/react";
+import { useState } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+
 export function MemberSignup() {
-  return null;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [nickName, setNickName] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+  const toast = useToast();
+  const navigate = useNavigate();
+
+  function handleClick() {
+    setIsLoading(true);
+    axios
+      .post("/api/member/signup", { email, password, nickName })
+      .then((res) => {
+        toast({
+          status: "success",
+          description: "회원 가입이 완료되었습니다.",
+          position: "top",
+        });
+        // todo : 로그인 화면으로 이동
+        navigate("/");
+      })
+      .catch((err) => {
+        if (err.response.status === 400) {
+          toast({
+            status: "error",
+            description: "입력값이 유효하지 않습니다.",
+            position: "top",
+          });
+        } else {
+          toast({
+            status: "error",
+            description: "회원가입 중 문제가 발생하였습니다.",
+            position: "top",
+          });
+        }
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
+  }
+
+  return (
+    <Box>
+      <Box>회원 가입</Box>
+      <Box>
+        <Box>
+          <FormControl>
+            <FormLabel>이메일</FormLabel>
+            <InputGroup>
+              <Input onChange={(e) => setEmail(e.target.value)} />
+              <InputRightElement w={"75px"} mr={2}>
+                <Button size={"sm"}>중복확인</Button>
+              </InputRightElement>
+            </InputGroup>
+          </FormControl>
+        </Box>
+        <Box>
+          <FormControl>
+            <FormLabel>암호</FormLabel>
+            <Input onChange={(e) => setPassword(e.target.value)} />
+          </FormControl>
+        </Box>
+        <Box>
+          <FormControl>
+            <FormLabel>별명</FormLabel>
+            <Input onChange={(e) => setNickName(e.target.value)} />
+          </FormControl>
+        </Box>
+        <Box>
+          <Button
+            isLoading={isLoading}
+            colorScheme={"blue"}
+            onClick={handleClick}
+          >
+            가입
+          </Button>
+        </Box>
+      </Box>
+    </Box>
+  );
 }
