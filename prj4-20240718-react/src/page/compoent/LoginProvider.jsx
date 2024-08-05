dea120fcfa8a24226c3678a05fe80e356af44ef2
kdw9805 <kdw686968@gmail.com>
2024-08-05 12:03:50 +0900

    새로고침 후에도 로그인 유지

--- prj4-20240718-react/src/page/compoent/LoginProvider.jsx
+++ prj4-20240718-react/src/page/compoent/LoginProvider.jsx
@@ -1,4 +1,4 @@
-import React, { createContext, useState } from "react";
+import React, { createContext, useEffect, useState } from "react";
 import { jwtDecode } from "jwt-decode";
 
 export const LoginContext = createContext(null);
@@ -7,6 +7,14 @@ export function LoginProvider({ children }) {
   const [email, setEmail] = useState("");
   const [nickName, setNickName] = useState("");
   const [expired, setExpired] = useState(0);
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    if (token === null) {
+      return;
+    }
+    login(token);
+  }, []);
 
   // isLoggedIn
   function isLoggedIn() {
