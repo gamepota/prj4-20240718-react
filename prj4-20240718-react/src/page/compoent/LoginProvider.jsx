933e4491b81d520e131f32eda8f5f54d4bd92614
kdw9805 <kdw686968@gmail.com>
2024-08-05 12:03:00 +0900

    LoginProvider.jsx 수정

--- prj4-20240718-react/src/page/compoent/LoginProvider.jsx
+++ prj4-20240718-react/src/page/compoent/LoginProvider.jsx
@@ -1,5 +1,52 @@
-import React from "react";
+import React, { createContext, useState } from "react";
+import { jwtDecode } from "jwt-decode";
+
+export const LoginContext = createContext(null);
 
 export function LoginProvider({ children }) {
-  return <LoginContext.Provider value={null}>{children}</LoginContext.Provider>;
+  const [email, setEmail] = useState("");
+  const [nickName, setNickName] = useState("");
+  const [expired, setExpired] = useState(0);
+
+  // isLoggedIn
+  function isLoggedIn() {
+    return Date.now() < expired * 1000;
+  }
+
+  // hasEmail
+  function hasEmail(param) {
+    return email === param;
+  }
+
+  // login
+  function login(token) {
+    localStorage.setItem("token", token);
+    const payload = jwtDecode(token);
+    setExpired(payload.exp);
+    setEmail(payload.sub);
+    setNickName(payload.nickName);
+  }
+
+  // logout
+  function logout() {
+    localStorage.removeItem("token");
+    setExpired(0);
+    setEmail("");
+    setNickName("");
+  }
+
+  return (
+    <LoginContext.Provider
+      value={{
+        email: email,
+        nickName: nickName,
+        login: login,
+        logout: logout,
+        isLoggedIn: isLoggedIn,
+        hasEmail: hasEmail,
+      }}
+    >
+      {children}
+    </LoginContext.Provider>
+  );
 }
--- prj4-20240718-react/src/page/member/MemberLogin.jsx
+++ prj4-20240718-react/src/page/member/MemberLogin.jsx
@@ -9,7 +9,7 @@ import {
 import { useContext, useState } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
-import { LoginContext } from "../../component/LoginProvider.jsx";
+import { LoginContext } from "../compoent/LoginProvider.jsx";
 
 export function MemberLogin() {
   const [email, setEmail] = useState("");
