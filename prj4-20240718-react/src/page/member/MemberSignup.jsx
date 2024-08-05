3058d2071f3620d899bce83a3cb4901ebc70dd69
kdw9805 <kdw686968@gmail.com>
2024-08-05 09:35:16 +0900

    이메일 닉네임 사용가능 한지 확인, 암호 확인 - 입력사항 확인시 버튼 활성화, 이메일 형식 제한, 벼명에 공백 제한

--- prj4-20240718-react/src/page/member/MemberSignup.jsx
+++ prj4-20240718-react/src/page/member/MemberSignup.jsx
@@ -2,6 +2,7 @@ import {
   Box,
   Button,
   FormControl,
+  FormHelperText,
   FormLabel,
   Input,
   InputGroup,
@@ -15,8 +16,13 @@ import { useNavigate } from "react-router-dom";
 export function MemberSignup() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [passwordCheck, setPasswordCheck] = useState("");
   const [nickName, setNickName] = useState("");
   const [isLoading, setIsLoading] = useState(false);
+  const [isCheckedEmail, setIsCheckedEmail] = useState(false);
+  const [isCheckedNickName, setIsCheckedNickName] = useState(false);
+  const [isValidEmail, setIsValidEmail] = useState(false);
+
   const toast = useToast();
   const navigate = useNavigate();
 
@@ -30,20 +36,20 @@ export function MemberSignup() {
           description: "회원 가입이 완료되었습니다.",
           position: "top",
         });
-        // todo : 로그인 화면으로 이동
+
         navigate("/");
       })
       .catch((err) => {
         if (err.response.status === 400) {
           toast({
             status: "error",
-            description: "입력값이 유효하지 않습니다.",
+            description: "입력값을 확인해 주세요.",
             position: "top",
           });
         } else {
           toast({
             status: "error",
-            description: "회원가입 중 문제가 발생하였습니다.",
+            description: "회원 가입 중 문제가 발생하였습니다.",
             position: "top",
           });
         }
@@ -51,6 +57,83 @@ export function MemberSignup() {
       .finally(() => {
         setIsLoading(false);
       });
+  }
+
+  function handleCheckEmail() {
+    axios
+      .get(`/api/member/check?email=${email}`)
+      .then((res) => {
+        toast({
+          status: "warning",
+          description: "사용할 수 없는 이메일입니다.",
+          position: "top",
+        });
+      }) // 이미 있는 이메일 (사용 못함)
+      .catch((err) => {
+        if (err.response.status === 404) {
+          // 사용할 수 있는 이메일
+          toast({
+            status: "info",
+            description: "사용할 수 있는 이메일입니다.",
+            position: "top",
+          });
+          setIsCheckedEmail(true);
+        }
+      })
+      .finally();
+  }
+
+  function handleCheckNickName() {
+    axios
+      .get(`/api/member/check?nickName=${nickName}`)
+      .then((res) => {
+        toast({
+          status: "warning",
+          description: "사용할 수 없는 별명입니다.",
+          position: "top",
+        });
+      })
+      .catch((err) => {
+        if (err.response.status === 404) {
+          toast({
+            status: "info",
+            description: "사용할 수 있는 별명입니다.",
+            position: "top",
+          });
+          setIsCheckedNickName(true);
+        }
+      })
+      .finally();
+  }
+
+  const isCheckedPassword = password === passwordCheck;
+
+  let isDisabled = false;
+
+  if (!isCheckedPassword) {
+    isDisabled = true;
+  }
+
+  if (
+    !(
+      email.trim().length > 0 &&
+      password.trim().length > 0 &&
+      nickName.trim().length > 0
+    )
+  ) {
+    isDisabled = true;
+  }
+
+  if (!isCheckedEmail) {
+    isDisabled = true;
+  }
+
+  if (!isCheckedNickName) {
+    isDisabled = true;
+  }
+
+  if (!isValidEmail) {
+    isDisabled = true;
   }
 
   return (
@@ -61,11 +144,33 @@ export function MemberSignup() {
           <FormControl>
             <FormLabel>이메일</FormLabel>
             <InputGroup>
-              <Input onChange={(e) => setEmail(e.target.value)} />
-              <InputRightElement w={"75px"} mr={2}>
-                <Button size={"sm"}>중복확인</Button>
+              <Input
+                type={"email"}
+                value={email}
+                onChange={(e) => {
+                  setEmail(e.target.value.trim());
+                  setIsCheckedEmail(false);
+                  setIsValidEmail(!e.target.validity.typeMismatch);
+                }}
+              />
+              <InputRightElement w={"75px"} mr={1}>
+                <Button
+                  isDisabled={!isValidEmail || email.trim().length == 0}
+                  onClick={handleCheckEmail}
+                  size={"sm"}
+                >
+                  중복확인
+                </Button>
               </InputRightElement>
             </InputGroup>
+            {isCheckedEmail || (
+              <FormHelperText>이메일 중복확인을 해주세요.</FormHelperText>
+            )}
+            {isValidEmail || (
+              <FormHelperText>
+                올바른 이메일 형식으로 작성해 주세요.
+              </FormHelperText>
+            )}
           </FormControl>
         </Box>
         <Box>
@@ -76,8 +181,37 @@ export function MemberSignup() {
         </Box>
         <Box>
           <FormControl>
+            <FormLabel>암호확인</FormLabel>
+            <Input onChange={(e) => setPasswordCheck(e.target.value)} />
+            {isCheckedPassword || (
+              <FormHelperText>암호가 일치하지 않습니다.</FormHelperText>
+            )}
+          </FormControl>
+        </Box>
+        <Box>
+          <FormControl>
             <FormLabel>별명</FormLabel>
-            <Input onChange={(e) => setNickName(e.target.value)} />
+            <InputGroup>
+              <Input
+                value={nickName}
+                onChange={(e) => {
+                  setNickName(e.target.value.trim());
+                  setIsCheckedNickName(false);
+                }}
+              />
+              <InputRightElement w={"75px"} mr={1}>
+                <Button
+                  isDisabled={nickName.trim().length == 0}
+                  size={"sm"}
+                  onClick={handleCheckNickName}
+                >
+                  중복확인
+                </Button>
+              </InputRightElement>
+            </InputGroup>
+            {isCheckedNickName || (
+              <FormHelperText>별명 중복확인을 해주세요.</FormHelperText>
+            )}
           </FormControl>
         </Box>
         <Box>
@@ -85,6 +219,7 @@ export function MemberSignup() {
             isLoading={isLoading}
             colorScheme={"blue"}
             onClick={handleClick}
+            isDisabled={isDisabled}
           >
             가입
           </Button>
